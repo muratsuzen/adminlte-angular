34c29e159e6180ad5b1b0e0ec6e49529df79aaed
Murat Suzen <muratsuzen@gmail.com>
2019-10-01 10:46:36 +0300

    create repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# ScmApp
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.5.
---
+++ src/app/layout/asidenavbar/asidenavbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-asidenavbar',
+  templateUrl: './asidenavbar.component.html',
+  styleUrls: ['./asidenavbar.component.css']
+})
+export class AsidenavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
